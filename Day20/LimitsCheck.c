84075cbfe9dbb52f65f74f6832e8cdd402e1af42
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2021-04-27 12:27:36 +0530

    Create LimitsCheck.c

---
+++ Day20/LimitsCheck.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+
+    unsigned int min,max;
+    min = 0;
+    max = UINT_MAX;
+    printf("Minimum value of unsigned int : %d\d",min);
+    printf("Maximum value of unsigned int : %u",max);
+
+    return 0;
+}
