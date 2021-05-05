3d47536c92a5eab09a9380d06fb3d3d1aa872f05
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2021-05-05 19:13:22 +0530

    Create Bitonic.c

---
+++ Day28/Bitonic.c
@@ -0,0 +1,44 @@
+// Bitonic Array - Increasing to Decreasing/ Decreasing to Increasing
+#include <stdio.h>
+
+int main()
+{
+    int SIZE, ind, flag = 0,itr;
+    scanf("%d",&SIZE);
+    int arr[SIZE];
+    
+    for(ind=0;ind< SIZE;ind++){
+        scanf("%d",&arr[ind]);
+    }
+    
+    if(arr[0] < arr[1]){
+        for(itr=0;arr[itr]<arr[itr+1] && itr<SIZE-1;itr++);
+        if(itr == SIZE-1){
+            flag = 1;
+        }
+        else
+        for(ind=itr;ind<SIZE-1;ind++){
+            if(arr[ind] < arr[ind+1]){
+                flag++;
+                break;
+            }
+        }
+    }
+    else{
+        for(itr=0;arr[itr]>arr[itr+1] && itr<SIZE-1;itr++);
+        
+        if(itr == SIZE-1){
+            flag = 1;
+        }
+        else
+        for(ind=itr;ind<SIZE-1;ind++){
+            if(arr[ind] > arr[ind+1]){
+                flag++;
+                break;
+            }
+        }
+    }
+    (flag == 0 )?printf("Bitonic"):printf("Not Bitonic");
+    
+    return 0;
+}
