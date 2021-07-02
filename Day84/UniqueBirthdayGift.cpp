b6467c4bc5c12a2bf5b65afdb4d13eb5de6f55cb
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2021-07-02 18:46:15 +0530

    Update UniqueBirthdayGift.cpp

--- Day84/UniqueBirthdayGift.cpp
+++ Day84/UniqueBirthdayGift.cpp
@@ -1,3 +1,10 @@
+/*
+He selected two numbers, N and K and decided to write down on paper all integer arrays of length K
+(in form a[1], a[2], ..., a[K]), where every number a[i] is in range from 1 to N, and, moreover, a[i+1] is
+divisible by a[i] (where 1 < i <= K), and give you this paper as a birthday present.
+Alex is very patient, so he managed to do this. Now you're wondering, how many different arrays are
+written down on this paper?
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
