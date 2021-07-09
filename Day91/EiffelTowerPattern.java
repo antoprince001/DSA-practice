189fef17b037a048838e3cca37c491085681b3f4
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2021-07-09 20:07:20 +0530

    Create EiffelTowerPattern.java

---
+++ Day91/EiffelTowerPattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main{
+
+  public static void main(String args[]){
+  
+    Scanner s = new Scanner(System.in);
+    int n= s.nextInt();
+    
+    for(int i=0;i<=n;i++){
+      for(int j=n;j>=i;j--){
+        int x = i*(i-1);
+        
+        while(x-- != 0){
+          System.out.println("%d",i);
+        }
+        System.out.println();
+      }
+    }
+    
+  }
+}
