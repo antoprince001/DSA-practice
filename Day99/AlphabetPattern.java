1a08d6e882732d65c908e5717db86a15b8ead6ef
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2021-07-17 19:51:14 +0530

    Create AlphabetPattern.java

---
+++ Day99/AlphabetPattern.java
@@ -0,0 +1,25 @@
+/*
+a z
+b y
+c x
+d w
+e v
+f u
+g t
+h s
+i r
+j q
+k p
+l o
+m n
+*/
+import java.util.*;
+public class Main
+{
+    
+	public static void main(String[] args) {
+		for(int i=0;i<13;i++){
+		    System.out.println((char)('a'+i) +" "+(char)('z'-i));
+		}
+	}
+}
